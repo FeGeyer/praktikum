f39a98d94ea6d017bb584b9eb03dcf4f6112b656
Rune <rune.dominik@udo.edu>
2016-11-19 20:07:52 +0100

    Jetzt neu! Mit bestimmt 50% mehr Grafiken

--- V204/plot_statisch.py
+++ V204/plot_statisch.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = (10, 8)
-plt.rcParams['font.size'] = 16
+plt.rcParams['font.size'] = 10
 
 
 Nr, s_statisch, T1_statisch, T4_statisch, T5_statisch, T8_statisch = np.genfromtxt('Statisch.txt', unpack=True)
 
-
-plt.ylabel("Temperatur in Celsius")
-plt.xlabel("Zeit in s")
+#plt.title("Temperaturverlauf der fernen Thermoelemente")
+plt.ylabel('Temperatur ($^\circ$C)')
+plt.xlabel("Zeit (s)")
 plt.plot(s_statisch, T1_statisch, 'r--', label="Messing breit")
 plt.plot(s_statisch, T4_statisch, 'y--', label="Messing dünn")
 plt.plot(s_statisch, T5_statisch, 'g--', label="Aluminium")
@@ -16,3 +16,5 @@ plt.plot(s_statisch, T8_statisch, 'b--', label="Edelstahl")
 #plt.plot(x_plot, f(x_plot, *params), 'b-', label='Regression', linewidth=3)
 plt.legend(loc="best")
 plt.savefig('Statisch.pdf')
+
+plt.tight_layout()

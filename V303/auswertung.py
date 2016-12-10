6e3b65a84822f5a9eda44b9abebd62ddf92de255
Rune <rune.dominik@udo.edu>
2016-12-10 17:09:50 +0100

    Erster Teil der Auswertung fertig

--- V303/auswertung.py
+++ V303/auswertung.py
@@ -23,16 +23,19 @@ deltaphi = ufloat(params[1], errors[1])
 
 print(Anull)
 print(deltaphi)
+np.savetxt('Teil1.txt', np.column_stack([(Phase / 180), Uunv, Uver]),fmt="%.2f")
 #plot
 x_plot = np.linspace(0, 2*np.pi)
 
 plt.figure(0)
-plt.title("U als Funktion von $\phi$")
+plt.title("Auftragen von U gegen $\phi$ und Regression")
 
 plt.ylabel('$U/V$')
 plt.xlabel("$\phi/rad$")
-plt.plot((Phase / 180)*np.pi, Uunv/Gain2u3, 'r+', label="$U($$\phi$$)$")
+plt.plot((Phase / 180)*np.pi, Uunv/Gain2u3, 'r+', label="Messwerte")
 plt.plot(x_plot, f(x_plot, *params), 'b-', label='Regression')
+plt.xticks([0, np.pi / 2, np.pi, 3 * np.pi / 2, 2 * np.pi],
+           [r"$0$", r"$\frac{1}{2}\pi$", r"$\pi$", r"$\frac{3}{2}\pi$", r"$2\pi$"])
 plt.legend(loc="best")
 plt.tight_layout
 plt.savefig('UvonPhi.pdf')

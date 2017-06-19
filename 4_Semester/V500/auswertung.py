3e853610bf3e685fe87253d4ee79227007c407b2
RuneDominik <rune.dominik@udo.edu>
2017-06-19 13:54:32 +0200

    etwas weiter

--- 4_Semester/V500/auswertung.py
+++ 4_Semester/V500/auswertung.py
@@ -19,6 +19,12 @@ UGeV, IGenA = np.genfromtxt('gelb.txt', unpack=True)
 UGrV, IGrnA = np.genfromtxt('gruen.txt', unpack=True)
 UUVV, IUVnA = np.genfromtxt('uv.txt', unpack=True)
 
+IB1nA = IB1nA * 10**(-9)
+IB2nA = IB2nA * 10**(-9)
+IGenA = IGenA * 10**(-9)
+IGrnA = IGrnA * 10**(-9)
+IUVnA = IUVnA * 10**(-9)
+
 UB1V = UB1V * (-1)
 UB2V = UB2V * (-1)
 UGeV = UGeV * (-1)
@@ -28,57 +34,56 @@ UUVV = UUVV * (-1)
 def f(U, m, n):
     return U*m + n
 
-paramsB1, covarianceB1 = curve_fit(f, np.sqrt(IB1nA), UB1V)
+paramsB1, covarianceB1 = curve_fit(f, UB1V[2:9], np.sqrt(IB1nA[2:9]))
 errorsB1 = np.sqrt(np.diag(covarianceB1))
 mB1 = ufloat(paramsB1[0], errorsB1[0])
 nB1 = ufloat(paramsB1[1], errorsB1[1])
 
-paramsB2, covarianceB2 = curve_fit(f, np.sqrt(IB2nA), UB2V)
+paramsB2, covarianceB2 = curve_fit(f, UB2V[1:8], np.sqrt(IB2nA[1:8]))
 errorsB2 = np.sqrt(np.diag(covarianceB2))
 mB2 = ufloat(paramsB2[0], errorsB2[0])
 nB2 = ufloat(paramsB2[1], errorsB2[1])
 
-paramsGe, covarianceGe = curve_fit(f, np.sqrt(IGenA), UGeV)
+paramsGe, covarianceGe = curve_fit(f, UGeV[2:7], np.sqrt(IGenA[2:7]))
 errorsGe = np.sqrt(np.diag(covarianceGe))
 mGe = ufloat(paramsGe[0], errorsGe[0])
 nGe = ufloat(paramsGe[1], errorsGe[1])
 
-paramsGr, covarianceGr = curve_fit(f, np.sqrt(IGrnA), UGrV)
+paramsGr, covarianceGr = curve_fit(f, UGrV[1:10], np.sqrt(IGrnA[1:10]))
 errorsGr = np.sqrt(np.diag(covarianceGr))
 mGr = ufloat(paramsGr[0], errorsGr[0])
 nGr = ufloat(paramsGr[1], errorsGr[1])
 
-paramsUV, covarianceUV = curve_fit(f, np.sqrt(IUVnA), UUVV)
+paramsUV, covarianceUV = curve_fit(f, UUVV[2:7], np.sqrt(IUVnA[2:7]))
 errorsUV = np.sqrt(np.diag(covarianceUV))
 mUV = ufloat(paramsUV[0], errorsUV[0])
 nUV = ufloat(paramsUV[1], errorsUV[1])
 
 print("Ug:")
-UgB1 = nB1
-UgB2 = nB2
-UgGe = nGe
-UgGr = nGr
-UgUV = nUV
-print("Gelb: ", UgGe)
-print("Grün: ", UgGr)
-print("Blau1: ", UgB1)
-print("Blau2: ", UgB2)
-print("UV: ", UgUV)
-
+UgB1 = (-1) * nB1 / mB1
+UgB2 = (-1) * nB2 / mB2
+UgGe = (-1) * nGe / mGe
+UgGr = (-1) * nGr / mGr
+UgUV = (-1) * nUV / mUV
+print("Gelb: ", UgGe , mGe, nGe)
+print("Grün: ", UgGr , mGr, nGr)
+print("Blau1: ", UgB2, mB2, nB2)
+print("Blau2: ", UgB1, mB1, nB2)
+print("UV: ", UgUV, mUV, nUV)
 
 # Plot
 x1plot = np.linspace(-0.2, 0.85)
 
 plt.figure(1)
-plt.ylim(0, 0.75)
-plt.xlim(-0.2, 0.4)
+plt.ylim(0, 0.000015)
+plt.xlim(-0.2, 0.85)
 plt.xlabel(r"$U / \mathrm{V}$")
-plt.ylabel(r"$I / \mathrm{mA}$")
-plt.plot(np.sqrt(IB1nA),UB1V,'b.', label="Blau 1")
-plt.plot(np.sqrt(IB2nA),UB2V,'k.', label="Blau 2")
-plt.plot(np.sqrt(IGenA),UGeV,'y.', label="Gelb")
-plt.plot(np.sqrt(IGrnA),UGrV,'g.', label="Grün")
-plt.plot(np.sqrt(IUVnA),UUVV,'r.', label="UV")
+plt.ylabel(r"$\sqrt{I / \mathrm{nA}}$")
+plt.plot(UB1V, np.sqrt(IB1nA),'b.', label="Blau 1")
+plt.plot(UB2V, np.sqrt(IB2nA),'k.', label="Blau 2")
+plt.plot(UGeV, np.sqrt(IGenA),'y.', label="Gelb")
+plt.plot(UGrV, np.sqrt(IGrnA),'g.', label="Grün")
+plt.plot(UUVV, np.sqrt(IUVnA),'r.', label="UV")
 
 plt.plot(x1plot, f(x1plot, *paramsB1), 'b', label="")
 plt.plot(x1plot, f(x1plot, *paramsB2), 'k', label="")
@@ -101,9 +106,11 @@ plt.savefig("Photostrom.pdf")
 #-------------------------------------------------------------------------------
 
 #[B1, B2, Ge, Gr, UV]
-lam = np.array([407, 435, 578, 546, 365])
+lam = np.array([436, 434, 579, 546, 405])
 lam = lam * 10 ** (-9)
 nu = const.c/lam
+
+print(nu)
 
 Ug = unp.nominal_values(np.array([UgB1, UgB2, UgGe, UgGr, UgUV]))
 Ugerr = unp.std_devs(np.array([UgB1, UgB2, UgGe, UgGr, UgUV]))

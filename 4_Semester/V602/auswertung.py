d4bf4fd2a06776e180cabf5689d2a4b389ad15c6
Felix Geyer <felix.geyer@tu-dortmund.de>
2017-07-06 14:00:50 +0200

    a

--- 4_Semester/V602/auswertung.py
+++ 4_Semester/V602/auswertung.py
@@ -21,12 +21,12 @@ plt.tight_layout()
 plt.savefig("emission.pdf")
 
 # Einlesen und in Energie umwandeln
-# Germanium
 d = 201.4 *10**(-12) #m
+# Germanium
 thetaGe1, IGe = np.genfromtxt('germanium.txt', unpack = True)
 thetaGe1 = thetaGe1/2
 EGe = const.h * const.c /(2*d*np.sin(2*np.pi*thetaGe1/360))/const.e
-
+np.savetxt('germaniumE.txt', np.column_stack([EGe*10**(-3), IGe]),fmt="%.1f")
 
 plt.figure(2)
 ax = plt.gca()
@@ -40,8 +40,79 @@ plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("germanium.pdf")
 
-np.savetxt('germaniumE.txt', np.column_stack([EGe*10**(-3), IGe]),fmt="%.3f")
+#Strontium
+thetaSr1, ISr = np.genfromtxt('strontium.txt', unpack = True)
+thetaSr1 = thetaSr1/2
+ESr = const.h * const.c /(2*d*np.sin(2*np.pi*thetaSr1/360))/const.e
+np.savetxt('strontiumE.txt', np.column_stack([ESr*10**(-3), ISr]),fmt="%.1f")
 
+plt.figure(3)
+ax = plt.gca()
+ax.invert_xaxis()
+plt.xlim(19.8, 13.6)
+plt.ylim(30, 170)
+plt.xlabel(r"$E / \mathrm{keV}$")
+plt.ylabel(r"$I / \mathrm{N/s}$")
+ax.plot(ESr*10**(-3), ISr, 'r-', label="Messwerte")
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig("strontium.pdf")
+
+#Brom
+thetaBr1, IBr = np.genfromtxt('brom.txt', unpack = True)
+thetaBr1 = thetaBr1/2
+EBr = const.h * const.c /(2*d*np.sin(2*np.pi*thetaBr1/360))/const.e
+np.savetxt('bromE.txt', np.column_stack([EBr*10**(-3), IBr]),fmt="%.1f")
+
+plt.figure(4)
+ax = plt.gca()
+ax.invert_xaxis()
+plt.xlim(16.2, 11.8)
+plt.ylim(14, 48)
+plt.xlabel(r"$E / \mathrm{keV}$")
+plt.ylabel(r"$I / \mathrm{N/s}$")
+ax.plot(EBr*10**(-3), IBr, 'r-', label="Messwerte")
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig("brom.pdf")
+
+# Zink
+thetaZn1, IZn = np.genfromtxt('Zink.txt', unpack = True)
+thetaZn1 = thetaZn1/2
+EZn = const.h * const.c /(2*d*np.sin(2*np.pi*thetaZn1/360))/const.e
+np.savetxt('zinkE.txt', np.column_stack([EZn*10**(-3), IZn]),fmt="%.1f")
+
+plt.figure(5)
+ax = plt.gca()
+ax.invert_xaxis()
+plt.xlim(10.6, 8.5)
+plt.ylim(50, 1200)
+plt.xlabel(r"$E / \mathrm{keV}$")
+plt.ylabel(r"$I / \mathrm{N/s}$")
+ax.plot(EZn*10**(-3), IZn, 'r-', label="Messwerte")
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig("zink.pdf")
+
+# Zirkonium
+thetaZr1, IZr = np.genfromtxt('strontium.txt', unpack = True)
+thetaZr1 = thetaZr1/2
+EZr = const.h * const.c /(2*d*np.sin(2*np.pi*thetaZr1/360))/const.e
+np.savetxt('strontiumE.txt', np.column_stack([EZr*10**(-3), IZr]),fmt="%.1f")
+
+plt.figure(6)
+ax = plt.gca()
+ax.invert_xaxis()
+plt.xlim(19.7,13.6)
+plt.ylim(33, 166)
+plt.xlabel(r"$E / \mathrm{keV}$")
+plt.ylabel(r"$I / \mathrm{N/s}$")
+ax.plot(EZr*10**(-3), IZr, 'r-', label="Messwerte")
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig("strontium.pdf")
+
+# Emission
 thetaCu = thetaCu/2
 maxEnergie10 = const.h * const.c /(2*d*np.sin(2*np.pi*5/360))/const.e
 #maxEnergie104 = const.h * const.c /(2*d*np.sin(2*np.pi*5.2/360))/const.e

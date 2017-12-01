2c0d4066e117b9c8f06ef1598448e19c1c6d7579
RuneDominik <rune.dominik@udo.edu>
2017-12-01 22:28:28 +0100

    Keine Lust mehr

--- BFP/V27/auswertung.py
+++ BFP/V27/auswertung.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 # import uncertainties.unumpy as unp
 from uncertainties import ufloat
 # from scipy.optimize import curve_fit
-# from scipy.stats import stats
-# import scipy.constants as const
+from scipy.stats import stats
+import scipy.constants as const
 plt.rcParams['figure.figsize'] = (10, 8)
 plt.rcParams['font.size'] = 18
 
@@ -38,3 +38,41 @@ plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("B_Feld.pdf")
 plt.clf()
+
+# Einlesen der aus den Bildern gewonnenen Daten
+ds_Rot_0, dels_Rot_0 = np.genfromtxt('Rot0.txt', unpack=True)
+ds_Blau_0, dels_Blau_0 = np.genfromtxt('blau0.txt', unpack=True)
+ds_Blau_90, dels_Blau_90 = np.genfromtxt('blau90.txt', unpack=True)
+
+I_Rot_0 = 10
+I_Blau_0 = 5.5
+I_Blau_90 = 14
+
+Disp_Geb_Bl = 26.95 * 10**(-12)
+Disp_Geb_Ro = 48.91 * 10**(-12)
+
+
+# Umrechnen in Wellenlängenänderung
+def del_lambda(del_s, delta_s, Disp):
+    return 0.5*(del_s/delta_s)*Disp
+
+
+def mg(lam, B):
+    return const.h * const.c / (lam*B*const.value("Bohr magneton"))
+
+
+def B(I):
+    return (x_3*I**3+x_2*I**2+x_1*I+x_0)*10**(-3)
+
+
+lam_Rot = del_lambda(dels_Rot_0, ds_Rot_0, Disp_Geb_Ro)
+lam_Rot_mean = ufloat(np.mean(lam_Rot), stats.sem(lam_Rot))
+lam_Blau0 = del_lambda(dels_Blau_0, ds_Blau_0, Disp_Geb_Bl)
+lam_Blau0_mean = ufloat(np.mean(lam_Blau0), stats.sem(lam_Blau0))
+lam_Blau90 = del_lambda(dels_Blau_90, ds_Blau_90, Disp_Geb_Bl)
+lam_Blau90_mean = ufloat(np.mean(lam_Blau90), stats.sem(lam_Blau90))
+
+print(lam_Rot_mean, lam_Blau0_mean, lam_Blau90_mean)
+
+mg_Rot = mg(lam_Rot_mean, B(I_Rot_0))
+print(mg_Rot)

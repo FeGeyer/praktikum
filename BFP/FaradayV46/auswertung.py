6849082effeedd4ebe2e3e3dfd571908a327de73
Felix Geyer <felix.geyer@tu-dortmund.de>
2017-12-14 15:52:52 +0100

    Soweit bis Diskussion fertig

--- BFP/FaradayV46/auswertung.py
+++ BFP/FaradayV46/auswertung.py
@@ -23,20 +23,20 @@ d2 = 1.296*10**(-3)
 d1 = 1.36*10**(-3)
 #Werte einelsen l = Wellenläng. tb= teta mit bfeld , to= teta ohne (b/o)bm= bogenmin mit und ohne bfeld
 #hochreines GaAg
-hl , htb , hbbm , hto , hobm = np.genfromtxt('hochreinesGaAs.txt', unpack = True)
+hl , htb , hbbm , hto , hobm = np.genfromtxt('hochreinesGaAs.txt', unpack = True, dtype='float64')
 #1. n-dotiertes GaAs
-l1 , tb1, bbm1 , to1 , obm1 = np.genfromtxt('1.n-dotiertesGaAs.txt', unpack = True)
+l1 , tb1, bbm1 , to1 , obm1 = np.genfromtxt('1.n-dotiertesGaAs.txt', unpack = True, dtype='float64')
 #2. n-dotiertes GaAs
-l2  , tb2 , bbm2 , to2 , obm2 = np.genfromtxt('2.n-dotiertesGaAs.txt', unpack = True)
+l2  , tb2 , bbm2 , to2 , obm2 = np.genfromtxt('2.n-dotiertesGaAs.txt', unpack = True, dtype='float64')
 #jetz noch die Winkel um und zusammenrechnen. dann die ohne bFeld von den mit BFeld abziehen. Alle Messwerte in ein Plot mit 'o-'
 #Winkel zusammenrechnen eine bogenmin = (1°/60) Grad
 u = 1/60 #u der kleine Umrechnungsfaktor, weil faul
-htb+= hbbm*u
-hto+= hobm*u
-tb1+= bbm1*u
-to1+= obm1*u
-tb2+= bbm2*u
-to2+= obm2*u
+htb += hbbm*u
+hto += hobm*u
+tb1 += bbm1*u
+to1 += obm1*u
+tb2 += bbm2*u
+to2 += obm2*u
 # The True Teta ausrechnen t mit BFeld - t ohne
 ht = 0.5*(htb - hto)
 t1 = 0.5*(tb1 - to1)
@@ -45,9 +45,9 @@ ht = abs(ht)
 t1 = abs(t1)
 t2 = abs(t2)
 #Umrechnen der Wellenlängen
-#hl*=10**-6
-#l1*=10**-6
-#l2*=10**-6
+# hl*=10**-6
+# l1*=10**-6
+# l2*=10**-6
 #Differenzzwischen der hochreinen und den zwei Proben
 D1 = abs(ht/hd-t1/d1)
 D2 = abs(ht/hd-t2/d2)
@@ -68,33 +68,47 @@ b1 = ufloat(params1[1], errors1[1])
 m2 = ufloat(params2[0], errors2[0])
 b2 = ufloat(params2[1], errors2[1])
 
-x = np.linspace(1, 7)
-plt.plot(hl**2, D1, 'r+', label="Messwerte 1 - Ideal")
+x = np.linspace(1, 7, 10000)
+plt.plot(hl**2, D1, 'r+', label=r"$\theta_1$")
 plt.plot(x, fitf1(x, *params1), 'r-', label="Regression 1")
-plt.plot(hl**2, D2, 'b+', label="Messwerte 2 - Ideal")
+plt.plot(hl**2, D2, 'b+', label=r"$\theta_2$")
 plt.plot(x, fitf1(x, *params2), 'b-', label="Regression 2")
 plt.xlabel(r"$\lambda^2 / \mathrm{\mu m}^2$")
-plt.ylabel(r"$\frac{\theta}{L} / \mathrm{\frac{1}{m}}$")
+plt.ylabel(r"$\theta_\mathrm{diff} / \mathrm{\frac{1}{m}}$")
 plt.tight_layout()
+plt.grid()
 plt.legend(loc="best")
 plt.savefig("Test.pdf")
 plt.clf()
 
-# print(m2)
-# print('Hildegard1:', unp.sqrt(N1/(m1*10**12)))
-# print('Hildegard2:', unp.sqrt(C2/(m2*10**12)))
-print('Hildegard1/me:', unp.sqrt((N1/(m1*10**-12)))/me)
-print('Hildegard2/me:', unp.sqrt((N2/(m2*10**-12)))/me)
+plt.plot(hl**2, ht, 'r+', label="hochreines GaAs")
+plt.plot(hl**2, t1, 'b+', label="1. n-dotiertes GaAs")
+plt.plot(hl**2, t2, 'g+', label="2. n-dotiertes GaAs")
+plt.xlabel(r"$\lambda^2 / \mathrm{\mu m}^2$")
+plt.ylabel(r"$\theta_\mathrm{norm} / \mathrm{\frac{1}{m}}$")
+plt.tight_layout()
+plt.grid()
+plt.legend(loc="best")
+plt.savefig("b.pdf")
+plt.clf()
+
+
+print(np.mean(hl))
+print("Erste Steigung: ", m1, b1)
+print("Zweite Steigung: ", m2, b2)
+print('Hildegard1/me:', unp.sqrt((N1/(m1*10**12))))
+print('Hildegard2/me:', unp.sqrt((N2/(m2*10**12))))
+print('Hildegard1/me:', unp.sqrt((N1/(m1*10**12)))/me)
+print('Hildegard2/me:', unp.sqrt((N2/(m2*10**12)))/me)
 m = np.array([unp.sqrt((N1/(m1*10**12)))/me, unp.sqrt((N2/(m2*10**12)))/me])
 print(m)
 print("Mittelwert: ", np.mean(m))
-# H = np.mean([unp.sqrt((C1/(a*10**12)))/me,unp.sqrt((C2/(b*10**12)))/me])
-# print('Mittlere Hildegard:' ,H)
-# print('Relf:',relf(0.067,H))
 # #Tabelle
-np.savetxt('TexTabellen/HGaAstab.txt',np.column_stack([(hl*10**(-6)),ht,(ht/hd)]),
-           delimiter=' & ',newline= r' \\'+'\n' )
-np.savetxt('TexTabellen/1.nGaAstab.txt',np.column_stack([(l1*10**(-6)),t1,(t1/d1)]),
-           delimiter=' & ',newline= r' \\'+'\n' )
-np.savetxt('TexTabellen/2.nGaAstab.txt',np.column_stack([(l2*10**(-6)),t2,(t2/d2)]),
-           delimiter=' & ',newline= r' \\'+'\n' )
+np.savetxt('TexTabellen/HGaAstab.txt',np.column_stack([hl,ht,(ht/hd)]),
+           delimiter=' & ',newline= r' \\'+'\n', fmt='%.2f')
+np.savetxt('TexTabellen/1.nGaAstab.txt',np.column_stack([l1,t1,(t1/d1)]),
+           delimiter=' & ',newline= r' \\'+'\n', fmt='%.2f')
+np.savetxt('TexTabellen/2.nGaAstab.txt',np.column_stack([l2,t2,(t2/d2)]),
+           delimiter=' & ',newline= r' \\'+'\n', fmt='%.2f')
+np.savetxt('TexTabellen/Differenz2.txt',np.column_stack([D1, D2]),
+           delimiter=' & ',newline= r' \\'+'\n', fmt='%.2f')

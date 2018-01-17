c504a9dd9e5b1116c331ca4eb25fe9188f47348a
Felix Geyer <felix.geyer@tu-dortmund.de>
2018-01-17 15:52:11 +0100

    lol

--- BFP/MyonenV01/auswertung.py
+++ BFP/MyonenV01/auswertung.py
@@ -15,11 +15,23 @@ dt, N = np.genfromtxt('verz.txt', unpack=True)
 daten = np.genfromtxt('Daten/Daten.Spe', unpack=True)
 kal1 = np.genfromtxt("Daten/Kal_1_micro_second.Spe", unpack=True)
 kal01 = np.genfromtxt("Daten/Kal_01_micro_second.Spe", unpack=True)
+dt = dt - 20    # Zentrieren
+N *= 10
+N -= 130
+hoehe = np.mean(N[4:16])
+print()
+print()
+print("Halbwertsbreite: ", 29)
+
 
 plt.figure(1)
-plt.ylabel(r"$\theta_D / T$")
-plt.xlabel(r"$c_V$ / J$\,$Mol$^{-1}\,$K$^{-1}$")
-plt.plot(dt, N, 'rx', label="Tabellierte Werte")
+plt.ylabel(r"$N(t) \, / \, (10\mathrm{s})^{-1}$")
+plt.xlabel(r"$\mathrm{d}t \, / \, \mathrm{ns}$")
+plt.errorbar(dt, N, yerr=np.sqrt(N), fmt='kx', label="Messwerte")
+# plt.plot(dt, N, 'r.', label="Messwerte")
+plt.axhline(y=hoehe, xmin=0.15, xmax=0.75, label="Plateau")
+plt.axhline(y=hoehe/2, xmin=0.07, xmax=0.85, color="red",
+            label="Halbwertsbreite")
 plt.grid()
 plt.legend(loc="best")
 plt.tight_layout()
@@ -47,7 +59,7 @@ x = np.linspace(0, 200)
 plt.plot(kanal, kal_t, 'rx', label="Daten")
 plt.plot(x, linear(x, *params_kal), 'b--', label="Regression")
 plt.xlabel("Kanal")
-plt.ylabel(r"$T_{VZ}$")
+plt.ylabel(r"$T_{VZ} \, / \, \mathrm{\mu s}$")
 plt.xlim(0, 200)
 plt.tight_layout()
 plt.legend(loc="best")
@@ -61,11 +73,12 @@ Startimpulse = 3061879
 Startimpulse = ufloat(Startimpulse, np.sqrt(Startimpulse))
 n = Startimpulse/messdauer
 Ts = 20*10**(-6)    # Sekunden
-print(n)
 Nf = Startimpulse*n*Ts*unp.exp(-n*Ts)
 Nf_kanal = Nf/len(daten)
-print(Nf)
-print(Nf_kanal)
+print("-------------------")
+print(Startimpulse)
+print("Fehlmessungen: ", Nf)
+print("Untergrundrate: ", Nf_kanal)
 # Umrechnung Kanäle in Zeit
 kanaele = np.arange(0, 512, 1)
 zeiten1 = linear(kanaele, *params_kal)
@@ -89,6 +102,7 @@ N_0 = ufloat(params_fit[0], errors_fit[0])
 l = ufloat(params_fit[1], errors_fit[1])
 U = ufloat(params_fit[2], errors_fit[2])
 
+print("-------------------")
 print("Lambda: ", l)
 print("Lebensdauer: ", 1/l)
 print("Untergrundrate: ", U)
@@ -102,3 +116,9 @@ plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig('fit.pdf')
 plt.clf()
+
+# Tabellen
+np.savetxt('Tabellen/tvz.txt', np.column_stack([dt, N]),
+           delimiter=' & ', newline=r' \\'+'\n', fmt="%.0f")
+np.savetxt('Tabellen/kal.txt', np.column_stack([kal_t, kanal]),
+           delimiter=' & ', newline=r' \\'+'\n', fmt="%.0f")

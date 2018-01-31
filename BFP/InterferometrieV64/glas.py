84711da524b22e83fc5e74cf8547b64c908646a1
RuneDominik <rune.dominik@udo.edu>
2018-01-31 15:55:58 +0100

    Tabellen sind da

--- BFP/InterferometrieV64/glas.py
+++ BFP/InterferometrieV64/glas.py
@@ -14,14 +14,39 @@ def n(lam, N, T, t):
     return (1-(lam * N)/(2*T*0.175*t))**(-1)
 
 
-n1 = ufloat(np.mean(n(lam, R1, T, theta)), stats.sem(n(lam, R1, T, theta)))
-n2 = ufloat(np.mean(n(lam, R2, T, theta)), stats.sem(n(lam, R2, T, theta)))
-n3 = ufloat(np.mean(n(lam, R3, T, theta)), stats.sem(n(lam, R3, T, theta)))
-n4 = ufloat(np.mean(n(lam, R4, T, theta)), stats.sem(n(lam, R4, T, theta)))
-n5 = ufloat(np.mean(n(lam, R5, T, theta)), stats.sem(n(lam, R5, T, theta)))
+n1_mean = ufloat(np.mean(n(lam, R1, T, theta)), stats.sem(n(lam, R1, T, theta)))
+n2_mean = ufloat(np.mean(n(lam, R2, T, theta)), stats.sem(n(lam, R2, T, theta)))
+n3_mean = ufloat(np.mean(n(lam, R3, T, theta)), stats.sem(n(lam, R3, T, theta)))
+n4_mean = ufloat(np.mean(n(lam, R4, T, theta)), stats.sem(n(lam, R4, T, theta)))
+n5_mean = ufloat(np.mean(n(lam, R5, T, theta)), stats.sem(n(lam, R5, T, theta)))
+
+n1 = n(lam, R1, T, theta)
+n2 = n(lam, R2, T, theta)
+n3 = n(lam, R3, T, theta)
+n4 = n(lam, R4, T, theta)
+n5 = n(lam, R5, T, theta)
+
+# np.append(n1, n1_mean)
+# np.append(n2, n2_mean)
+# np.append(n3, n3_mean)
+# np.append(n4, n4_mean)
+# np.append(n5, n5_mean)
+# np.append(theta, np.nan)
+
+print(n1)
+
+np.savetxt('TexTabellen/glas.txt', np.column_stack([
+        theta * 180/np.pi,
+        n1,
+        n2,
+        n3,
+        n4,
+        n5
+        ]), delimiter=' & ', newline=r' \\'+'\n',
+        fmt='%.0f & %.2f & %.2f & %.2f & %.2f & %.2f')
 
 
-n_array = [n1, n2, n3, n4, n5]
+n_array = [n1_mean, n2_mean, n3_mean, n4_mean, n5_mean]
 n_best = np.mean(n_array)
 
 print(n_best)
--- BFP/InterferometrieV64/luft.py
+++ BFP/InterferometrieV64/luft.py
@@ -1,11 +1,12 @@
 import numpy as np
 from uncertainties import ufloat
 from uncertainties import unumpy
+import uncertainties.unumpy as unp
 from scipy.stats import stats
 
 N = np.genfromtxt('luft.txt', unpack=True)
 
-N = unumpy.uarray(N, 4)
+N = unumpy.uarray(N, 2)
 
 def n(N, lam, L):
     return N * lam / L + 1
@@ -18,3 +19,11 @@ n_array = n(N, lam, L)
 
 n_best = np.mean(n_array)
 print(n_best)
+
+np.savetxt('TexTabellen/luft.txt', np.column_stack([
+        unp.nominal_values(N),
+        unp.std_devs(N),
+        unp.nominal_values(n_array),
+        unp.std_devs(n_array),
+        ]), delimiter=' & ', newline=r' \\'+'\n',
+        fmt='%.0f & %.0f & %.5f & %.5f')

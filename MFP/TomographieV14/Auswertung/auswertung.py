39da4401d66ec18371306d758073062b49a56242
Felix Geyer <felix.geyer@tu-dortmund.de>
2018-10-15 14:09:26 +0200

    Auswertungsdatei bis Plots und Daten
    Altprotokolle in einen anderen Ordner verschoben, die Daten bearbeitet
    und die Auswertungsdatei mit den Plots erstellt

--- MFP/TomographieV14/Auswertung/auswertung.py
+++ MFP/TomographieV14/Auswertung/auswertung.py
@@ -6,49 +6,37 @@ plt.rcParams['font.size'] = 18
 # Einlesen der Daten
 # Umrechnungsfaktor für Energie
 umrechnung = 9.323944
+
+# Liste für Wüfelnummern
+liste = [1, 2, 4]
 # Leermessung
 
-daten = np.genfromtxt('../Daten/Quellspektrum/Quellspektrum_300.Spe', unpack=True)
+daten = np.genfromtxt('../Daten/Quellspektrum/Quellspektrum_echt_300.Spe', unpack=True)
 x = np.linspace(0, 511, 512)
 plt.plot(x*umrechnung, daten/3, 'r--',label="Daten")
 plt.axvline(662, color='black',label=r"$662 \ \mathrm{keV}$")
 plt.xlim(0, 1000)
-plt.ylim(-50, 3500)
+plt.ylim(-50, 3700)
 plt.yticks([0, 500, 1000, 1500, 2000, 2500, 3000], ['0', '0,5', '1', '1,5', '2', '2,5', '3'])
 plt.xlabel(r"$\mathrm{Energie} \ / \ \mathrm{keV}$")
 plt.ylabel(r'$\mathrm{Anzahl \ 1000 \ Ereignisse} \ / \ 100 \, \mathrm{s}$')
 plt.legend(loc='best')
-plt.savefig('quellmessung.pdf')
+plt.savefig('Plots/Quellmessung/quellmessung.pdf')
 plt.clf()
 # Würfel 1
+for j in liste:
+    for i in range(12):
+        daten = np.genfromtxt('../Daten/Wuerfel%1.0u/I%1.0u.Spe' % (j, i+1), unpack=True)
+        x = np.linspace(0, 511, 512)
+        plt.plot(x*umrechnung, daten, 'r--',label="Daten")
+        plt.axvline(662, color='black',label=r"$662 \ \mathrm{keV}$")
+        plt.xlim(0, 1000)
+        plt.ylim(-50, 3500)
+        plt.yticks([0, 500, 1000, 1500, 2000, 2500, 3000], ['0', '0,5', '1', '1,5', '2', '2,5', '3'])
+        plt.xlabel(r"$\mathrm{Energie} \ / \ \mathrm{keV}$")
+        plt.ylabel(r'$\mathrm{Anzahl \ 1000 \ Ereignisse} \ / \ 100 \, \mathrm{s}$')
+        plt.legend(loc='best')
+        plt.savefig('Plots/Wuerfel%1.0u/wuerfel%1.0u_%1.0u.pdf' % (j, j, i+1))
+        plt.clf()
 
-for i in range(12):
-    daten = np.genfromtxt('../Daten/Wuerfel%1.0u/I%1.0u.Spe' % (1, i+1), unpack=True)
-    x = np.linspace(0, 511, 512)
-    plt.plot(x*umrechnung, daten, 'r--',label="Daten")
-    plt.axvline(662, color='black',label=r"$662 \ \mathrm{keV}$")
-    plt.xlim(0, 1000)
-    plt.ylim(-50, 3500)
-    plt.yticks([0, 500, 1000, 1500, 2000, 2500, 3000], ['0', '0,5', '1', '1,5', '2', '2,5', '3'])
-    plt.xlabel(r"$\mathrm{Energie} \ / \ \mathrm{keV}$")
-    plt.ylabel(r'$\mathrm{Anzahl \ 1000 \ Ereignisse} \ / \ 100 \, \mathrm{s}$')
-    plt.legend(loc='best')
-    plt.savefig('wuerfel1_%1.0u.pdf' % (i+1))
-    plt.clf()
-# for i in range(3):
-#     for j in range(12):
-#         daten = np.genfromtxt('Daten/Wuerfel%1.0u/I%1.0u' % (i+1, i+1))
-#
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I1.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I2.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I3.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I4.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I5.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I6.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I7.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I8.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I9.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I1.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I1.Spe')
-#daten_1_1 = np.genfromtxt('Daten/Wuerfel1/I1.Spe')
 print('Alles ausgeführt!')

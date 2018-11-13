e23a7c44151b4f80c1e6d75049780ee71ba8248a
Rune Dominik <rune.dominik@udo.edu>
2018-11-13 17:45:51 +0100

    erster Versuch, noch nicht richtig

--- MFP/DebeyeV41/Auswertung/auswertung.py
+++ MFP/DebeyeV41/Auswertung/auswertung.py
@@ -1,6 +1,6 @@
-from uncertainties import ufloat
-from uncertainties import unumpy
 from scipy.signal import find_peaks
+from scipy.stats import sem
+import latticeconstants as lat
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -23,13 +23,13 @@ GreyValuePeaks = Metall.GreyValue[MetallPeaks]
 # Get Distance from peaks
 MetallPeaks = MetallPeaks*(18/(len(Metall.Pixel)+6))
 
-plt.figure()
-plt.plot(Metall.Distance, Metall.GreyValue, ls='--', color='blue')
-plt.plot(MetallPeaks, GreyValuePeaks, color='black', ls='', marker='o')
-plt.xlabel(r"$r / \mathrm{cm}$")
-plt.ylabel('inverser Grauwert')
-plt.xlim(0, 18)
-plt.show()
+# plt.figure()
+# plt.plot(Metall.Distance, Metall.GreyValue, ls='--', color='blue')
+# plt.plot(MetallPeaks, GreyValuePeaks, color='black', ls='', marker='o')
+# plt.xlabel(r"$r / \mathrm{cm}$")
+# plt.ylabel('inverser Grauwert')
+# plt.xlim(0, 18)
+# plt.show()
 
 # Distance is equal to 180° -> 1cm equals 10°
 # Bragg: 2dsin(theta)=n*lambda
@@ -40,14 +40,12 @@ plt.show()
 lam = 1.54093*10**(-10)
 R = 57.3*10**(-3)
 
-PeakAngle = MetallPeaks/(2*R)
-PeakAngle = np.abs(PeakAngle-180)
+PeakAngle = MetallPeaks*10
+PeakAngle = np.abs(180-PeakAngle)
+PeakAngle = np.sort(PeakAngle)
 
-print(PeakAngle)
-
-# The first reflexes are due to the existence of to K-alpha lines, so correct for it
-
-# theta = np.arctan((PeakAngle[0]-PeakAngle[1])*(1.5417*10**(-10))/(1.54478*10**(-10)-lam))
+# The first two reflexes are due to the existence of two K-alpha lines,
+# so correct for it
 theta = (PeakAngle[0]-PeakAngle[1])/2
 PeakAngle = np.insert(PeakAngle, 0, PeakAngle[0]-theta)
 PeakAngle = np.delete(PeakAngle, [1, 2])
@@ -55,19 +53,95 @@ PeakAngle = np.delete(PeakAngle, [1, 2])
 print(PeakAngle)
 print(theta)
 
-d = lam/(2*np.sin(PeakAngle/2))
+# convert the angles to interplanar distance according to braggs law
+d = lam/(2*np.sin(PeakAngle/2 * np.pi / 180))
 
-# Possible Millerindizes for fcc and bcc
-############
-# 1: bcc: h+k+l gerade
-# 2: fcc: h,k,l alle gerade oder ungerade
-############
+# Compute possible millerindizes for fcc and bcc
+h_bcc, k_bcc, l_bcc = lat.bcc(7)
+h_fcc, k_fcc, l_fcc = lat.fcc(7)
 
-# nenner1 = np.sqrt(h1**2 + k1**2 + l1**2)
-# nenner2 = np.sqrt(h2**2 + k2**2 + l2**2)
-#
-# a1 = d*nenner1
-# a2 = d*nenner2
+n_bcc = np.sqrt(h_bcc**2 + k_bcc**2 + l_bcc**2)
+n_fcc = np.sqrt(h_fcc**2 + k_fcc**2 + l_fcc**2)
 
-print(a1)
-print(a2)
+
+def find_best_fit(evaluate_value, test_array, i):
+
+    '''
+    evaluate_value: float, value, for witch the best fitting value for n
+    has to be evaluated.
+    test_array: array of floats, possible values for evaluate_value
+    i: int, index of currently evaluated PeakAngle
+
+    Function to find the best fitting value, therfor the value, where the
+    euclidian distance between evaluate_value and the evaluated test_array
+    value is minimal.
+
+    Returns best fit and distance to evaluated value (residuum res)
+    '''
+
+    res = d[i]/np.abs(test_array-evaluate_value)
+    best_fit = test_array[d[i]/np.abs(test_array-evaluate_value) ==
+                          d[i]/np.abs(test_array-evaluate_value).min()]
+    best_res = res[d[i]/np.abs(test_array-evaluate_value) ==
+                   d[i]/np.abs(test_array-evaluate_value).min()]
+    return best_fit[0], best_res[0]
+
+# Combining Bragg and the interplanar distance latticeconstant relation gives
+# sin(theta_2)/sin(theta_1) = n_1/n_2 = c with n = np.sqrt(h**2+k**2+l**2)
+
+# initialize array for c
+c_bcc = np.zeros([len(PeakAngle)-1])
+
+# compute c values
+for angle_index in range(len(PeakAngle)-1):
+    c_bcc[angle_index] = np.sin(PeakAngle[angle_index+1] * np.pi / 180) / \
+                         np.sin(PeakAngle[angle_index] * np.pi / 180)
+
+# initialize field, where the n_values for each initial guess of n are
+# computeted iterative and stored
+n_search_field = np.zeros([len(PeakAngle), len(n_bcc)])
+
+# set initial guess on all possible n
+n_search_field[0, :] = n_bcc
+
+# iterativly compute following n
+for row in range(len(PeakAngle)-1):
+    for column in range(len(n_bcc)):
+        n_search_field[row+1, column] = n_search_field[row, column]/c_bcc[row]
+
+# seafty first, even if negative values should not happen
+# n_search_field = np.abs(n_search_field)
+print(c_bcc)
+
+# initialize fields for best fits and residdums, setting value of the initial
+# guess according to the formally used values
+n_best_field = np.zeros([len(PeakAngle), len(n_bcc)])
+n_best_field[0, :] = n_bcc
+n_res_field = np.zeros([len(PeakAngle), len(n_bcc)])
+n_res_field[0, :] = 0
+
+# compute best fits and risiduums, deltining formally used ns
+for column in range(len(n_bcc)):
+    n_bcc_help = n_bcc
+    help_list = n_bcc_help.tolist()
+    index = help_list.index(n_best_field[0, column])
+    n_bcc_help = np.delete(n_bcc_help, [index])
+    for row in range(len(PeakAngle)-1):
+        (n_best_field[row+1, column],
+            n_res_field[row+1, column]) = find_best_fit(n_search_field[row+1,
+                                                        column], n_bcc_help,
+                                                        row+1)
+        help_list = n_bcc_help.tolist()
+        index = help_list.index(n_best_field[row+1, column])
+        n_bcc_help = np.delete(n_bcc_help, [index])
+# compute mean of residuums per column and find minimal value
+res_sum = np.mean(n_res_field, axis=0)
+res_sem = sem(n_res_field, axis=0)
+res_sum_list = res_sum.tolist()
+best_index = res_sum_list.index(res_sum.min())
+best_sum = res_sum[best_index]
+best_sem = res_sem[best_index]
+
+print(n_best_field[:, best_index]**2)
+print(d/n_best_field[:, best_index])
+print(best_sum, best_sem)
--- MFP/DebeyeV41/Auswertung/latticeconstants.py
+++ MFP/DebeyeV41/Auswertung/latticeconstants.py
@@ -1,45 +1,86 @@
-import numpy as np
-
 # Possible Millerindizes for fcc and bcc
 ############
 # 1: bcc: h+k+l even
 # 2: fcc: h,k,l all even or odd
 ############
 # Therefor use the the modulo operation: n % 2 == {0: n even or 1: n odd}
+# max_value denotes the maximal value for h, k and l respectivly that should
+# be evaluated
 
-# Set maximum-value for witch the Millerindizes should be computed, meaning the
-# maximal value for h, k, and l
+import numpy as np
+import itertool as it
 
-max_value = 7
 
-# initialize lists fpr indizes
-h_bcc = []
-k_bcc = []
-l_bcc = []
+def find_double(array):
+	# implement function, that finds dublicates
+	# Nur schauen, ob die h, k, ls des nächsten eintrages gleich dem des aktuellen
+	# sind. ja -> index merken und einen weiter gehen und wieder schauen
+	# nein -> suche mit dem nächsten hkl ab dem übernächsten hkl weiter.
 
-h_fcc = []
-k_fcc = []
-l_fcc = []
+
+
+def sort(h, k, l):
+	array = np.empty([len(h), 3])
+	for i in range(len(h)):
+		row = np.array([h[i], k[i], l[i]])
+		array[i, :] = row.sort
+		return array
+
 
 # bcc
-print("bcc:")
-for h in range(max_value):
-	for k in range(max_value):
-		for l in range(max_value):
-			if (h+k+l) % 2 == 0 and (h+k+l) > 0:
-				print(h, k, l)
-				h_bcc.append(h)
-				k_bcc.append(k)
-				l_bcc.append(l)
+def bcc(max_value):
+	h_bcc = np.empty([1])
+	k_bcc = np.empty([1])
+	l_bcc = np.empty([1])
+	print("bcc:")
+	for l in range(max_value):
+		for k in range(max_value):
+			for h in range(max_value):
+				if (h+k+l) % 2 == 0 and (h+k+l) > 0:
+					h_bcc = np.append(h_bcc, h)
+					k_bcc = np.append(k_bcc, k)
+					l_bcc = np.append(l_bcc, l)
+	h_bcc = np.delete(h_bcc, [0])
+	k_bcc = np.delete(k_bcc, [0])
+	l_bcc = np.delete(l_bcc, [0])
+
+	combined_array = np.empty([len(h_bcc), 4])
+	combined_array[:, 0] = h_bcc
+	combined_array[:, 1] = k_bcc
+	combined_array[:, 2] = l_bcc
+	combined_array[:, 3] = np.sqrt(h_bcc**2 + k_bcc**2 + l_bcc**2)
+
+	combined_array[combined_array[:, 3].argsort()]
+
+	array = sort(h_bcc, k_bcc, l_bcc)
+
+	return combined_array[:, 0], combined_array[:, 1], combined_array[:, 2]
+
 
 # fcc
-print("fcc:")
-for h in range(max_value):
-	for k in range(max_value):
-		for l in range(max_value):
-			if (h % 2 + k % 2 + l % 2) == 0 or (h % 2 + k % 2 + l % 2) == 1:
-				if (h+k+l) > 0:
-					print(h, k, l)
-					h_fcc.append(h)
-					k_fcc.append(k)
-					l_fcc.append(l)
+def fcc(max_value):
+	h_fcc = np.empty([])
+	k_fcc = np.empty([])
+	l_fcc = np.empty([])
+	print("fcc:")
+	for l in range(max_value):
+		for k in range(max_value):
+			for h in range(max_value):
+				if (h % 2 + k % 2 + l % 2) == 0 or (h % 2 + k % 2 + l % 2) == 1:
+					if (h+k+l) > 0:
+						h_fcc = np.append(h_fcc, h)
+						k_fcc = np.append(k_fcc, k)
+						l_fcc = np.append(l_fcc, l)
+	h_fcc = np.delete(h_fcc, [0])
+	k_fcc = np.delete(k_fcc, [0])
+	l_fcc = np.delete(l_fcc, [0])
+
+	combined_array = np.empty([len(h_fcc), 4])
+	combined_array[:, 0] = h_fcc
+	combined_array[:, 1] = k_fcc
+	combined_array[:, 2] = l_fcc
+	combined_array[:, 3] = np.sqrt(h_fcc**2 + k_fcc**2 + l_fcc**2)
+
+	combined_array[combined_array[:, 3].argsort()]
+
+	return combined_array[:, 0], combined_array[:, 1], combined_array[:, 2]

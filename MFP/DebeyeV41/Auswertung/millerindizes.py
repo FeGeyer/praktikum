44aa9298239b67f3a96bbcbe8e84b5ccb6104da0
Rune Dominik <rune.dominik@udo.edu>
2018-11-14 14:02:40 +0100

    probably wrong angle, no linear fit possible

--- MFP/DebeyeV41/Auswertung/millerindizes.py
+++ MFP/DebeyeV41/Auswertung/millerindizes.py
@@ -1,15 +1,26 @@
 import numpy as np
-# Possible Millerindizes for fcc and bcc
-############
-# 1: bcc: h+k+l even
-# 2: fcc: h,k,l all even or odd
-############
-# Therefor use the the modulo operation: n % 2 == {0: n even or 1: n odd}
-# max_value denotes the maximal value for h, k and l respectivly that should
-# be evaluated
+'''
+Some functions to compute possible miller-tupel (h, k, l) for fcc-
+and bcc-lattice.
+
+Fuctions make use of the following selection rules:
+1: bcc: h+k+l even
+2: fcc: h,k,l all even or odd
+Therefor use the the modulo operation: n % 2 == {0: n even or 1: n odd}.
+'''
 
 
 def find_permutations(array):
+
+	'''
+	3darray: array of ints, array of the sorted millerindizes h, k, l
+
+	Funktion to find the index of permutated or doubled miller-tupel (h, k, l).
+	Needs row-wise sorted (h, k, l).
+
+	Returns the indizes of permutations and doubles.
+	'''
+
 	indize = np.empty([1])
 	for i in range(len(array)):
 		tested = array[i, :]
@@ -22,6 +33,15 @@ def find_permutations(array):
 
 
 def sort_rows(h, k, l):
+
+	'''
+	h, k, l: arrays of ints, arrays millerindizes.
+
+	Funktion to sort miller-tupels (h, k, l) row-wise.
+
+	Returns 3darray of row-wise sorted miller-tupel.
+	'''
+
 	array = np.empty([len(h), 3])
 	for i in range(len(h)):
 		row = -np.array([h[i], k[i], l[i]])
@@ -32,10 +52,19 @@ def sort_rows(h, k, l):
 
 # bcc
 def bcc(max_value):
+
+	'''
+	max_value: Maximal value for h, k, l respectively.
+
+	Funktion compute possible bcc millerindizes. Calls functions to sort out
+	permutations and doubles.
+
+	Returns arrays of ints with millerindizes for the bcc-lattice.
+	'''
+
 	h_bcc = np.empty([1])
 	k_bcc = np.empty([1])
 	l_bcc = np.empty([1])
-	print("bcc:")
 	for l in range(max_value):
 		for k in range(max_value):
 			for h in range(max_value):
@@ -79,10 +108,19 @@ def bcc(max_value):
 
 # fcc
 def fcc(max_value):
+
+	'''
+	max_value: Maximal value for h, k, l respectively.
+
+	Funktion compute possible fcc millerindizes. Calls functions to sort out
+	permutations and doubles.
+
+	Returns arrays of ints with millerindizes for the fcc-lattice.
+	'''
+
 	h_fcc = np.empty([])
 	k_fcc = np.empty([])
 	l_fcc = np.empty([])
-	print("fcc:")
 	for l in range(max_value):
 		for k in range(max_value):
 			for h in range(max_value):
@@ -108,8 +146,6 @@ def fcc(max_value):
 
 	combined_array = combined_array[combined_array[:, 3].argsort()]
 
-	print(combined_array)
-
 	indizes = find_permutations(combined_array[:, 0:3])
 
 	h_fcc = np.delete(combined_array[:, 0], indizes)
@@ -123,7 +159,5 @@ def fcc(max_value):
 	combined_array[:, 3] = np.sqrt(h_fcc**2 + k_fcc**2 + l_fcc**2)
 
 	combined_array = combined_array[combined_array[:, 3].argsort()]
-
-	print(combined_array)
 
 	return combined_array[:, 0], combined_array[:, 1], combined_array[:, 2]

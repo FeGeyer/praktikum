66648df92474d24057f80121abdaa29018b0e0e0
Felix Geyer <felix.geyer@tu-dortmund.de>
2018-12-11 17:19:44 +0100

    New experiment

---
+++ MFP/HeNeLaserV61/auswertung.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties.unumpy as unp
+import pandas as pd
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from scipy import constants
+
+# Use latex fonts and text
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+print('Alles ausgeführt!')
